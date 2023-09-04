39c4e3c069095b20e74288a001d743d57aee8484
s160420100 <s160420100@student.ubaya.ac.id>
2023-09-04 15:11:21 +0700

    init

---
+++ app/src/main/java/com/example/adv160320100week2/MainActivity.kt
@@ -0,0 +1,27 @@
+package com.example.adv160320100week2
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import androidx.navigation.NavController
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.NavigationUI
+
+class MainActivity : AppCompatActivity() {
+    private lateinit var navController: NavController
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        navController =
+            (supportFragmentManager.findFragmentById(R.id.hostFragment) as
+                    NavHostFragment).navController
+        NavigationUI.setupActionBarWithNavController(this, navController)
+    }
+
+    override fun onSupportNavigateUp(): Boolean {
+        return navController.navigateUp()
+    }
+}
